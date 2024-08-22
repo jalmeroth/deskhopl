64ec96107585a0eeafeef8a157cfd8c05c52437c
Jan Almeroth <jan@almeroth.com>
2024-08-22 10:17:39 +0200

    chore: print less

--- src/usb.c
+++ src/usb.c
@@ -5,8 +5,14 @@ bool send_tud_report(uint8_t instance, uint8_t report_id, uint8_t const *report,
   bool success = false;
   if (tud_ready()) {
     success = tud_hid_n_report(instance, report_id, report, len);
+    printf("x[report] instance %d report_id %d len %d\r\n", instance, report_id,
+           len);
+    for (uint8_t i = 0; i < len; i++) {
+      printf("%02x ", report[i]);
+    }
+    printf("\r\n");
     if (!success) {
-      printf("x[report] success: %s\r\n", success ? "true" : "false");
+      printf("success: %s\r\n", success ? "true" : "false");
     }
   } else {
     printf("x[report] tud not ready\r\n");
@@ -25,15 +31,7 @@ bool send_x_report(enum packet_type_e packet_type, uint8_t instance,
 
   if (BOARD_ROLE == global_state.active_output) {
     global_state.last_activity = time_us_64();
-    success = send_tud_report(instance, report_id, report, len);
-    if (!success) {
-      printf("x[send] instance %d report_id %d type %d size %d\r\n", instance,
-             report_id, packet_type, len);
-      for (uint8_t i = 0; i < len; i++) {
-        printf("%02x ", report[i]);
-      }
-      printf("\r\n");
-    }
+    send_tud_report(instance, report_id, report, len);
   } else {
     send_packet(instance, report_id, (uint8_t *)report, packet_type, len);
   }
